70ab0d44c690e4109622151068611c7fe5a9149a
Mexahoid <mirramax97@Mail.ru>
2018-04-02 21:30:28 +0300

    Initial commit

---
+++ src/main/java/mex/tdop/ConsoleController.java
@@ -0,0 +1,15 @@
+package mex.tdop;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsoleController implements CommandLineRunner {
+
+    @Override
+    public void run(String... args){
+        int i = 0;
+
+        i = 10;
+    }
+}
---
+++ src/main/java/mex/tdop/Main.java
@@ -0,0 +1,18 @@
+package mex.tdop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class Main {
+
+    public static void main(String[] args) {
+        //Menu();
+        SpringApplication.run(Main.class, args);
+    }
+}
